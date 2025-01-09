d700ddeb3155364bdf9f124646677dabc6149a29
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2025-01-08 21:24:43 -0600

    init

--- src/app/(DashboardLayout)/layout/shared/logo/FullLogo.tsx
+++ src/app/(DashboardLayout)/layout/shared/logo/FullLogo.tsx
@@ -1,16 +1,25 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import Logo from "/public/images/logos/dark-logo.svg";
+import Logo from "/public/images/logos/logo.png";
 import Logowhite from "/public/images/logos/light-logo.svg";
 import Link from "next/link";
 const FullLogo = () => {
   return (
     <Link href={"/"}>
       {/* Dark Logo   */}
-      <Image src={Logo} alt="logo" className="block dark:hidden rtl:scale-x-[-1]" />
+      <Image
+        src={Logo}
+        alt="logo"
+        width={220}
+        className="block dark:hidden rtl:scale-x-[-1] bg-contain"
+      />
       {/* Light Logo  */}
-      <Image src={Logowhite} alt="logo" className="hidden dark:block rtl:scale-x-[-1]" />
+      <Image
+        src={Logowhite}
+        alt="logo"
+        className="hidden dark:block rtl:scale-x-[-1]"
+      />
     </Link>
   );
 };
